d05c7f9da96b6f5e948886dbb2b393a2f961222b
imMaturana <maturana.dev@protonmail.com>
2023-10-04 14:01:38 -0300

    zsh: add some useful functions

---
+++ config/.zshrc.d/functions.zsh
@@ -0,0 +1,22 @@
+ex() {
+    for file in "$@"; do
+        case "$file" in
+            *.tar.gz|*.tar.xz|*.tgz|*.txz|*.tar)
+                tar zxvf "$file" ;;
+            *.zip|*.epub)
+                unzip "$file" ;;
+        esac
+    done
+}
+
+open() {
+    for file in "$@"; do
+        case "$file" in
+            *.opus|*.ogg|*.md4) mpv "$file" ;;
+            *.html) xdg-open "$file" ;;
+            *.pdf) zathura "$file" ;;
+            *.jpg|*.jpeg|*.png) imv "$file" ;;
+            *.txt|*.md) cat "$file" | less ;;
+        esac
+    done
+}
